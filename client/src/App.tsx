7af5ca388969fff8f6f916bdae7dbf0c206d37fd
omsant02 <omsantoshwar02@gmail.com>
2024-02-23 02:12:36 +0530

    connect wallet

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Layout, Row, Col } from "antd";
+
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <Row align="middle">
+          <Col span={10} offset={2}>
+            <h1>Our todolist</h1>
+          </Col>
+          <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
+            <WalletSelector />
+          </Col>
+        </Row>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
