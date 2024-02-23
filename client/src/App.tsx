ca25b7f1f559bf6baab177e6455d8cb56e4e14a3
omsant02 <omsantoshwar02@gmail.com>
2024-02-23 11:50:57 +0530

    submit data on chain

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { Layout, Row, Col, Button } from "antd";
+import { Layout, Row, Col, Button, Spin } from "antd";
 import { Aptos } from "@aptos-labs/ts-sdk";
 
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
-import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import {
+  useWallet,
+  InputTransactionData,
+} from "@aptos-labs/wallet-adapter-react";
 
-const aptos = new Aptos();
+export const aptos = new Aptos();
+
+export const moduleAddress =
+  "6dfcf8d4afabfb700e3e02246262a7dca2c6410e3326ceb97af47607b0bd46b8";
 
 function App() {
-  const { account } = useWallet();
+  const { account, signAndSubmitTransaction } = useWallet();
   const [accountHasList, setAccountHasList] = useState<boolean>(false);
+  const [transactionInProgress, setTransactionInProgress] =
+    useState<boolean>(false);
 
   const fetchList = async () => {
     if (!account) return [];
     // change this to be your module account address
-    const moduleAddress =
-      "0xcbddf398841353776903dbab2fdaefc54f181d07e114ae818b1a67af28d1b018";
+
     try {
       const todoListResource = await aptos.getAccountResource({
         accountAddress: account?.address,
@@ -25,6 +32,28 @@ function App() {
       setAccountHasList(true);
     } catch (e: any) {
       setAccountHasList(false);
+    }
+  };
+
+  const addNewList = async () => {
+    if (!account) return [];
+    setTransactionInProgress(true);
+    const transaction: InputTransactionData = {
+      data: {
+        function: `${moduleAddress}::todolist::create_list`,
+        functionArguments: [],
+      },
+    };
+    try {
+      // sign and submit transaction to chain
+      const response = await signAndSubmitTransaction(transaction);
+      // wait for transaction
+      await aptos.waitForTransaction({ transactionHash: response.hash });
+      setAccountHasList(true);
+    } catch (error: any) {
+      setAccountHasList(false);
+    } finally {
+      setTransactionInProgress(false);
     }
   };
 
@@ -44,19 +73,22 @@ function App() {
           </Col>
         </Row>
       </Layout>
-      {!accountHasList && (
-        <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
-          <Col span={8} offset={8}>
-            <Button
-              block
-              type="primary"
-              style={{ height: "40px", backgroundColor: "#3f67ff" }}
-            >
-              Add new list
-            </Button>
-          </Col>
-        </Row>
-      )}
+      <Spin spinning={transactionInProgress}>
+        {!accountHasList && (
+          <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+            <Col span={8} offset={8}>
+              <Button
+                onClick={addNewList}
+                block
+                type="primary"
+                style={{ height: "40px", backgroundColor: "#3f67ff" }}
+              >
+                Add new list
+              </Button>
+            </Col>
+          </Row>
+        )}
+      </Spin>
     </>
   );
 }
